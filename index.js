d6d4576aa22e0cf700030d6d29fa073ee8868042
Idan Stark <idanstark42@gmail.com>
2018-03-20 20:00:21 +0200

    Alot of clean up - now the code is clean

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const DEFAULT_SECRET = '321LEGO';
+const DEFAULT_TOKEN_EXPIRATION = 24 * 60 * 60 * 1000; // day
+const DEFAULT_HOME_ROUTE = '/';
+const COOKIE_KEY = 'user';
 
 var express = require('express'),
 var cookieParser = require('cookie-parser'),
@@ -6,28 +9,40 @@ var jwt = require('jsonwebtoken'),
 var config = require('config'),
 var router = express.Router();
 
-var secret = config.has('secret') ? config.get('secret') : DEFAULT_SECRET;
 var identityProviderUrl = config.get('idp');
+var secret = config.has('secret') ? config.get('secret') : DEFAULT_SECRET;
+var tokenExpiration = config.has('tokenExpiration') ? config.get('tokenExpiration') : DEFAULT_TOKEN_EXPIRATION;
+var homeRoute = config.has('homeRoute') ? config.get('homeRoute') : DEFAULT_HOME_ROUTE;
+var publicRoutes = ['/consume_token'];
+if(config.has('publicRoutes')) {
+  publicRoutes = publicRoutes.concat(config.get('publicRoutes'));
+}
 
 router.use(cookieParser());
 
 router.use((req, res, next) => {
-  if(req.url === '/consume_token') {
+
+  res.redirectToIdP = function() {
+    res.redirect(`${identityProviderUrl}/login?callbackUrl=${req.host}/consume_token`);
+  };
+
+});
+
+router.use((req, res, next) => {
+  if(publicRoutes.includes(req.url) || (req.cookie[COOKIE_KEY] && jwt.verify(req.cookie[COOKIE_KEY], secret))) {
   	next();
-  } else if(req.cookie['user'] && jwt.verify(req.cookie['user'], secret)) {
-    next();
   } else {
-    res.redirect(`${identityProviderUrl}/login?callback_url=${req.host}/consume_token`);
+    res.redirectToIdP();
   }
 });
 
 router.get('/consume_token', (req, res, next) => {
-  var auth_token = req.query['token'] || req.params['token'] || req.body['token'];
-  if(req.cookie['user'] && jwt.verify(req.cookie['user'], secret)) {
-    res.cookie('user', token, { maxAge: 24 * 60 * 60 * 1000 /* day */ });
-    res.redirect('/');
+  var token = req.query['token'] || req.params['token'] || req.body['token'];
+  if(token && jwt.verify(token, secret)) {
+    res.cookie(COOKIE_KEY, token, { maxAge: tokenExpiration });
+    res.redirect(homeRoute);
   } else {
-    res.redirect(`${identityProviderUrl}/login?callback_url=${req.host}/consume_token`);
+    res.redirectToIdP();
   }
 });
 
