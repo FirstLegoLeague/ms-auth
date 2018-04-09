99e1188b2692a01217cdea14f538e47eb577b7c5
Idan Stark <idanstark42@gmail.com>
2018-04-09 23:46:42 +0300

    Added eslint

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
-const DEFAULT_SECRET = '321LEGO';
-const DEFAULT_TOKEN_EXPIRATION = 24 * 60 * 60 * 1000; // day
-const DEFAULT_HOME_ROUTE = '/';
-const COOKIE_KEY = 'user';
-const HEADER_KEY = 'auth-token'; // Following the FIRST LEGO League System module standard v1.0
+'use strict'
 
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var jwt = require('jsonwebtoken');
-var config = require('config');
-var router = express.Router();
+const DEFAULT_SECRET = '321LEGO'
+const DEFAULT_TOKEN_EXPIRATION = 24 * 60 * 60 * 1000 // day
+const DEFAULT_HOME_ROUTE = '/'
+const COOKIE_KEY = 'user'
+const HEADER_KEY = 'auth-token' // Following the FIRST LEGO League System module standard v1.0
 
-var identityProviderUrl = config.get('idp');
-var secret = config.has('secret') ? config.get('secret') : DEFAULT_SECRET;
-var tokenExpiration = config.has('tokenExpiration') ? config.get('tokenExpiration') : DEFAULT_TOKEN_EXPIRATION;
-var homeRoute = config.has('homeRoute') ? config.get('homeRoute') : DEFAULT_HOME_ROUTE;
-var publicRoutes = ['/consume_token'];
-if(config.has('publicRoutes')) {
-  publicRoutes = publicRoutes.concat(config.get('publicRoutes'));
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+const router = express.Router()
+const identityProviderUrl = config.get('idp')
+const secret = config.has('secret') ? config.get('secret') : DEFAULT_SECRET
+const tokenExpiration = config.has('tokenExpiration') ? config.get('tokenExpiration') : DEFAULT_TOKEN_EXPIRATION
+const homeRoute = config.has('homeRoute') ? config.get('homeRoute') : DEFAULT_HOME_ROUTE
+let publicRoutes = ['/consume_token']
+if (config.has('publicRoutes')) {
+  publicRoutes = publicRoutes.concat(config.get('publicRoutes'))
 }
 
-router.use(cookieParser());
+router.use(cookieParser())
 
 router.use((req, res, next) => {
-
-  res.redirectToIdP = function() {
-    res.redirect(`${identityProviderUrl}/login?callbackUrl=${req.host}/consume_token`);
-  };
-
-});
+  res.redirectToIdP = function () {
+    res.redirect(`${identityProviderUrl}/login?callbackUrl=${req.host}/consume_token`)
+  }
+})
 
 router.use((req, res, next) => {
-  if(publicRoutes.includes(req.url)) {
-    next();
-    return;
+  if (publicRoutes.includes(req.url)) {
+    next()
+    return
   }
 
-  var existingAuthToken = req.get(HEADER_KEY) || req.cookies[COOKIE_KEY];
-  if(existingAuthToken && jwt.verify(existingAuthToken, secret)) {
-  	next();
-    return;
+  const existingAuthToken = req.get(HEADER_KEY) || req.cookies[COOKIE_KEY]
+  if (existingAuthToken && jwt.verify(existingAuthToken, secret)) {
+    next()
+    return
   }
-  
-  res.redirectToIdP();
-});
+  res.redirectToIdP()
+})
 
 router.get('/consume_token', (req, res, next) => {
-  var token = req.query['token'] || req.params['token'] || req.body['token'];
-  if(token && jwt.verify(token, secret)) {
-    res.cookie(COOKIE_KEY, token, { maxAge: tokenExpiration });
-    res.redirect(homeRoute);
+  const token = req.query['token'] || req.params['token'] || req.body['token']
+  if (token && jwt.verify(token, secret)) {
+    res.cookie(COOKIE_KEY, token, { maxAge: tokenExpiration })
+    res.redirect(homeRoute)
   } else {
-    res.redirectToIdP();
+    res.redirectToIdP()
   }
-});
+})
 
-module.export = router;
+module.export = router
