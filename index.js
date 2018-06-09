adb6db4ac668d2d95eb62f4ccfd1f8f7cf388fce
Idan Stark <idanstark42@gmail.com>
2018-06-09 22:56:09 +0300

    Changed the package to both authentication and authorization

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 
 exports.authenticationMiddleware = require('./lib/authentication_router').router
 exports.authenticationDevMiddleware = require('./lib/dev_router').router
+exports.authroizationMiddlware = require('./lib/authorization_router').router
