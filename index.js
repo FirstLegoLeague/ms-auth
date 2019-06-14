6ea54076d4444982da31711f70c33682c0e39a00
Idan Stark <idanstark42@gmail.com>
2019-06-14 18:07:00 +0300

    cleaning

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 exports.authenticationMiddleware = require('./lib/authentication_router').router
 exports.authenticationDevMiddleware = require('./lib/dev_router').router
 exports.authroizationMiddlware = require('./lib/authorization_router').router
